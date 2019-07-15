746f293ab56783639347fedd54aee26db641611a
jjeda <jjeda@naver.com>
2019-07-15 09:05:26 +0900

    jpa sample start

---
+++ jpa-sample-application/src/main/java/me/jjeda/jpasampleapplication/JpaSampleApplication.java
@@ -0,0 +1,13 @@
+package me.jjeda.jpasampleapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaSampleApplication.class, args);
+    }
+
+}
