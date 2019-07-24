5e9d843ad70da6a0fb71d8a009ef40a2d6328ce6
jjeda <jjeda@naver.com>
2019-07-24 11:04:49 +0900

    customal Annotation

--- rest-whiteship/src/test/java/me/jjeda/restwhiteship/events/EventControllerTests.java
+++ rest-whiteship/src/test/java/me/jjeda/restwhiteship/events/EventControllerTests.java
@@ -1,6 +1,7 @@
 package me.jjeda.restwhiteship.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.jjeda.restwhiteship.commons.TestDescription;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -35,6 +36,7 @@ public class EventControllerTests {
     ObjectMapper objectMapper;
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
         EventDto event = EventDto.builder() //Dto 를사용해서 예외값 처리하는경우
                 .name("Spring")
@@ -65,6 +67,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 받을 수 없는 값이 사용한 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100)
@@ -92,8 +95,31 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 비어있는 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto eventDto = EventDto.builder().build();
+
+        this.mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(this.objectMapper.writeValueAsString(eventDto)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @TestDescription("입력 값이 잘못된 경우에 에러가 발생하는 테스트")
+    public void createEvent_Bad_Request_Wrong_Input() throws Exception {
+        EventDto eventDto = EventDto.builder()
+                .name("Spring")
+                .desciption("REST API")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018,11,24,4,21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018,11,23,4,21))
+                .beginEventDateTime(LocalDateTime.of(2018,11,24,4,21))
+                .endEventDateTime(LocalDateTime.of(2018,11,23,4,21))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역")
+                .build();
 
         this.mockMvc.perform(post("/api/events")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
